dd83a5c01046cf0ccb49e969120bd7a0c3658049
Developer ✔ <josephburss97@gmail.com>
2020-09-18 15:37:08 -0500

    user and article getters and etc...

--- Agora/src/main/java/com/agora/models/Article.java
+++ Agora/src/main/java/com/agora/models/Article.java
@@ -1,5 +1,7 @@
 package com.agora.models;
 
+import java.util.Arrays;
+
 import javax.persistence.*;
 
 import lombok.Data;
@@ -7,8 +9,6 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "articles")
-@Data
-@NoArgsConstructor
 public class Article {
 	
 	@Id
@@ -22,5 +22,120 @@ public class Article {
 	private String publishedAt;
 	private String content;
 	private int status;
+	
+	public Article() {
+		super();
+	}
+	public Article(int id, User user, String description, byte[] image, String publishedAt, String content,
+			int status) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.description = description;
+		this.image = image;
+		this.publishedAt = publishedAt;
+		this.content = content;
+		this.status = status;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public byte[] getImage() {
+		return image;
+	}
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	public String getPublishedAt() {
+		return publishedAt;
+	}
+	public void setPublishedAt(String publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + Arrays.hashCode(image);
+		result = prime * result + ((publishedAt == null) ? 0 : publishedAt.hashCode());
+		result = prime * result + status;
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Article other = (Article) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (!Arrays.equals(image, other.image))
+			return false;
+		if (publishedAt == null) {
+			if (other.publishedAt != null)
+				return false;
+		} else if (!publishedAt.equals(other.publishedAt))
+			return false;
+		if (status != other.status)
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", user=" + user + ", description=" + description + ", image="
+				+ Arrays.toString(image) + ", publishedAt=" + publishedAt + ", content=" + content + ", status="
+				+ status + "]";
+	}
 
+	
+	
 }
--- Agora/src/main/java/com/agora/models/User.java
+++ Agora/src/main/java/com/agora/models/User.java
@@ -10,9 +10,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class User {
 	
 	@Id
@@ -26,5 +24,143 @@ public class User {
 	
 	@OneToMany(targetEntity = Article.class, mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	private Set<Article> articles;
+
+	public User() {
+		super();
+	}
+
+	public User(int id, String firstName, String lastName, String userName, String password, String email,
+			Set<Article> articles) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.articles = articles;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(Set<Article> articles) {
+		this.articles = articles;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((articles == null) ? 0 : articles.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (articles == null) {
+			if (other.articles != null)
+				return false;
+		} else if (!articles.equals(other.articles))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName
+				+ ", password=" + password + ", email=" + email + ", articles=" + articles + "]";
+	}
+	
+	
 
 }

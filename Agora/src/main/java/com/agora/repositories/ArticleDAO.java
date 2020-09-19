68282f368290d77a7229e3662090b2852bfd14f2
Max Paul <maxyapaul@gmail.com>
2020-09-19 15:42:36 -0400

    added article dao and service

---
+++ Agora/src/main/java/com/agora/repositories/ArticleDAO.java
@@ -0,0 +1,79 @@
+package com.agora.repositories;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.NoResultException;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+import com.agora.models.Article;
+import com.agora.util.HibernateUtil;
+
+@Repository
+public class ArticleDAO {
+	
+	public Set<Article> findAll() {
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+
+        Set<Article> result = session.createQuery("FROM Article a", Article.class)
+                .getResultStream()
+                .collect(Collectors.toSet());
+
+        tx.commit();
+
+        return result;
+    }
+	
+	public Article findArticleById(int ar_id) {
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+    try {
+        Article result = session.createQuery("FROM Article a where id = ?1", Article.class)
+                .setParameter(1, ar_id)
+                .getSingleResult();
+
+        tx.commit();
+        return result;
+    } catch(NoResultException e) {
+        e.printStackTrace();
+        tx.rollback();
+        return null;
+    }
+    
+	}
+
+
+    public void save(Article art) {
+        Session session = HibernateUtil.getSession();
+
+        Transaction tx = session.beginTransaction();
+
+        session.save(art);
+
+        tx.commit();
+    }
+
+    public void update(Article art) {
+        Session session = HibernateUtil.getSession();
+
+        Transaction tx = session.beginTransaction();
+
+        session.merge(art);
+
+        tx.commit();
+    }
+
+    public void delete(Article art) {
+        Session session = HibernateUtil.getSession();
+        Transaction tx = session.beginTransaction();
+
+        session.delete(art);
+
+        tx.commit();
+    }
+
+}
---
+++ Agora/src/main/java/com/agora/services/ArticleService.java
@@ -0,0 +1,44 @@
+package com.agora.services;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.agora.models.Article;
+import com.agora.repositories.ArticleDAO;
+
+@Service
+public class ArticleService {
+	
+	ArticleDAO articleDAO;
+
+    @Autowired
+    public ArticleService(ArticleDAO articleDAO) {
+        this.articleDAO = articleDAO;
+    }
+
+    public Set<Article> findAll() {
+        return articleDAO.findAll();
+    }
+
+    public void save(Article art) {
+        articleDAO.save(art);
+    }
+
+    public void update(Article art) {
+        articleDAO.update(art);
+    }
+
+    public Article findUserById(int id) { 
+    	return articleDAO.findArticleById(id); 
+    }
+    public void delete(Article art) { 
+    	articleDAO.delete(art); 
+    }
+
+    public Article checkUsername(Article art) { 
+    	return art; 
+    }
+
+}
